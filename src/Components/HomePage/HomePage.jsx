462f16b9c0bc052178937419309d5a9b08cf8696
Paskal Khadka <khadkapaskal2@gmail.com>
2025-02-14 17:53:49 +0545

    added features in homepage

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -8,6 +8,16 @@ import galaxybuds3 from '../../assets/GalaxyBuds3.jpeg'
 import galaxybuds3pro from '../../assets/GalaxyBuds3Pro.jpeg'
 import galaxywatch7 from '../../assets/GalaxyWatch7.jpeg'
 import galaxywatchultra from '../../assets/GalaxyWatchUltra.jpeg'
+import hero_image from '../../assets/banner.png'
+import buylaptops from '../../assets/BuyLaptops.webp'
+import buyPhone from '../../assets/buyPhone.webp'
+import buySmartWatches from '../../assets/BuySmartWatches.webp'
+import findnewphone from '../../assets/FindNewPhone.webp'
+import nearbystore from '../../assets/NearbyStores.webp'
+import recycle from '../../assets/Recycle.webp'
+import repairphone from '../../assets/RepairPhone.webp'
+import repairLaptop from '../../assets/RepairLaptop.webp'
+import sellPhone from '../../assets/sellPhone.webp'
 import './HomePage.css';
 
 function HomePage() {
@@ -191,6 +201,58 @@ function HomePage() {
                 <p className="different-equipment price">$348.60</p>
                 <p className="different-equipment discount"><del>$498.00</del> <span className="different-equipment save">Save $149.40</span></p>
                 <button className="different-equipment add-button">Add</button>
+            </div>
+        </div>
+    </section>
+
+
+
+
+   
+    <header className="banner-content-header">
+        <div className="banner-content-banner">
+            <img src={hero_image} alt="Valentine’s Day Sale Banner"/>
+        </div>
+    </header>
+
+    <section className="banner-content-services">
+        <h2>Our Services</h2>
+        <div className="banner-content-services-container">
+            <div className="banner-content-service">
+                <img src={sellPhone} alt="Sell Phone"/>
+                <p>Sell Phone</p>
+            </div>
+            <div className="banner-content-service">
+                <img src={buyPhone} alt="Buy Phone"/>
+                <p>Buy Phone</p>
+            </div>
+            <div className="banner-content-service">
+                <img src={buylaptops} alt="Buy Laptops"/>
+                <p>Buy Laptops</p>
+            </div>
+            <div className="banner-content-service">
+                <img src={repairphone} alt="Repair Phone"/>
+                <p>Repair Phone</p>
+            </div>
+            <div className="banner-content-service">
+                <img src={repairLaptop} alt="Repair Laptop"/>
+                <p>Repair Laptop</p>
+            </div>
+            <div className="banner-content-service">
+                <img src={findnewphone} alt="Find New Phone"/>
+                <p>Find New Phone</p>
+            </div>
+            <div className="banner-content-service">
+                <img src={nearbystore} alt="Nearby Stores"/>
+                <p>Nearby Stores</p>
+            </div>
+            <div className="banner-content-service">
+                <img src={buySmartWatches} alt="Buy Smartwatches"/>
+                <p>Buy Smartwatches</p>
+            </div>
+            <div className="banner-content-service">
+                <img src={recycle} alt="Recycle"/>
+                <p>Recycle</p>
             </div>
         </div>
     </section>
